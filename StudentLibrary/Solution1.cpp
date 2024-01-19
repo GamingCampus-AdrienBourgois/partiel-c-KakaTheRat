8352433f7f5c2be5bb3b53d710d59179cf8b0c31
unknown <kdifallah@gamingcampus.local>
2024-01-19 13:22:58 +0100

    Exo 1

--- StudentLibrary/Solution1.cpp
+++ StudentLibrary/Solution1.cpp
@@ -2,11 +2,36 @@
 
 // Don't fortget to enable the exercise in the SudentConfiguration.h file !
 #include "StudentConfiguration.h"
+
 #ifdef COMPILE_EXERCICE_1
+
+//* Fahrenheit to Kelvin K = 5 / 9 (°F - 32) + 273
 
 float Solution1::ConvertTemperature(float _value, TemperatureUnits _from, TemperatureUnits _to)
 {
-	return -1.0f;
+	float result = 0.0;
+	if (_from == TemperatureUnits::CELSIUS && _to == TemperatureUnits::FAHRENHEIT) {
+		result = 9 / 5 * (_value)+32;
+	}
+	else if(_from == TemperatureUnits::KELVIN && _to == TemperatureUnits::FAHRENHEIT) {
+		result = 9 / 5 * (_value - 273) + 32;
+	}
+	else if (_from == TemperatureUnits::KELVIN && _to == TemperatureUnits::FAHRENHEIT) {
+		result = 9 / 5 * (_value - 273) + 32;
+	}
+	else if (_from == TemperatureUnits::FAHRENHEIT && _to == TemperatureUnits::CELSIUS) {
+		result = 5 / 9 * (_value - 32);
+	}
+	else if (_from == TemperatureUnits::CELSIUS && _to == TemperatureUnits::KELVIN) {
+		result = _value + 273;
+	}
+	else if (_from == TemperatureUnits::KELVIN && _to == TemperatureUnits::CELSIUS) {
+		result = _value - 273;
+	}
+	else if (_from == TemperatureUnits::FAHRENHEIT && _to == TemperatureUnits::KELVIN) {
+		result = 5 / 9 * (_value - 32) + 273;
+	}
+	return result;
 }
 
 #endif
