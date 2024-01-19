cfd4e3c3aafcbd4992b3d084aaaa95b8cfca70bd
KakaTheRat <kais.difallah@gmail.com>
2024-01-19 14:48:59 +0100

    Exo 3

--- StudentLibrary/Solution3.cpp
+++ StudentLibrary/Solution3.cpp
@@ -1,4 +1,5 @@
 #include "Solution3.h"
+#include <algorithm>
 
 // Don't fortget to enable the exercise in the SudentConfiguration.h file !
 #include "StudentConfiguration.h"
@@ -9,7 +10,20 @@ void Solution3::SetWords(const std::vector<std::string>& _words)
 	words = _words;
 }
 
-void Solution3::SortWords() {}
+void Solution3::SortWords() {
+	if (words.empty()) {
+		throw std::exception("Words vector vide");
+	}
+	std::sort(words.begin(), words.end(), [](const std::string& a, const std::string& b) {
+		std::string aLower = a;
+		std::string bLower = b;
+		std::transform(aLower.begin(), aLower.end(), aLower.begin(), ::tolower);
+		std::transform(bLower.begin(), bLower.end(), bLower.begin(), ::tolower);
+
+		return aLower < bLower;
+		});
+		
+}
 
 std::vector<std::string> Solution3::GetSortedWords() const
 {
